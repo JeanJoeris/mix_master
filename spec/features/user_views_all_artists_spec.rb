08399ef010e9764792db9f2b1c87a3776fe68080
Jean Joeris <joeris.peter@gmail.com>
2016-09-01 18:34:52 -0600

    finished feature test for users viewing the artists index

--- spec/features/user_views_all_artists_spec.rb
+++ spec/features/user_views_all_artists_spec.rb
@@ -7,6 +7,15 @@ RSpec.feature "User views all artists" do
     # When I visit the artists index
     # Then I should see each artist's name
     # And each name should link to that artist's individual page
+    
+    the_wu = Artist.create(name: "Wu-Tang Clan", image_path: "http://images1.mtv.com/uri/mgid:uma:video:mtv.com:1104536?width=657&height=370&crop=true&quality=0.85")
+    queen = Artist.create(name: "Queen", image_path: "http://www.virginemirecords.com/wp-content/uploads/2015/10/QUEEN.jpg")
 
+    visit artists_path
+
+    within("div.artists_list") do
+      page { is_expected.to have_link('Wu-Tang Clan', href=artists_path(the_wu.id)) }
+      page { is_expected.to have_link('Queen', href=artists_path(queen.id)) }
+    end
   end
 end
