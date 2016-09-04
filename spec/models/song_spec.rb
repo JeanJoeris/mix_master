778c5ef13ec27ed2a75f06637788297e752da73a
Jean Joeris <joeris.peter@gmail.com>
2016-09-03 18:00:40 -0600

    updated edit form to check which songs a playlist possesses more simply

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "given correct data" do
+    it  "has all the right attributes" do
+      title = "Friday"
+      song = Song.new(title: title)
+
+      expect(song.title).to eq(title)
+    end
+  end
+
+  context "given partial data" do
+    it "requires a title" do
+      song = Song.new()
+
+      expect(song).to be_invalid
+    end
+  end
 end
