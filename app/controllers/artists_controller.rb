d8c4fa8b5810588e7edc4b145c8012f9dc6ef48c
Jean Joeris <joeris.peter@gmail.com>
2016-09-03 21:36:28 -0600

    added helper methods to controllers for looking up objects, and a errors partial

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,11 +1,12 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
+
 
   def index
     @artists = Artist.all
   end
 
   def show
-    @artist = Artist.find(params[:id])
   end
 
   def new
@@ -22,11 +23,9 @@ class ArtistsController < ApplicationController
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
     if @artist.update(artist_params)
       redirect_to @artist
     else
@@ -35,7 +34,6 @@ class ArtistsController < ApplicationController
   end
 
   def destroy
-    @artist = Artist.find(params[:id])
     @artist.destroy
     redirect_to artists_path
   end
@@ -44,6 +42,10 @@ class ArtistsController < ApplicationController
 
   def artist_params
     params.require(:artist).permit(:name, :image_path)
+  end
+
+  def set_artist
+    @artist = Artist.find(params[:id])
   end
 
 end
